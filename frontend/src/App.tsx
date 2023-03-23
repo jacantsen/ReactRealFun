48c02d3d9bdcc72b898dcf428bc202ed565c8d93
jacantsen <jacob.ac@sbcglobal.net>
2023-03-22 19:45:13 -0600

    Added college stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import {teams} from './colleges';
 import './App.css';
+class Team extends React.Component
+<{school: string, name:string, city:string, state:string}>{
+  
+  render(){
+    const oneTeam = this.props;
+    return(
+    <div>
+      <h1>{oneTeam.school}</h1>
+      <p>{oneTeam.name}</p>
+      <p>{[oneTeam.city, ",  ", oneTeam.state]}</p>
 
+    </div>
+    )
+  }
+}
+
+function Teamlist(){
+return (
+  <div>
+    {teams.map(oneTeam => <Team {...oneTeam}/>)}
+  </div>
+)
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the NCAA College Basketball InfoSite</h1>
+      <Teamlist/>
     </div>
+
   );
 }
 
